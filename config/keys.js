6f440ed6a935adecb70dc1ae429b728f290bcab9
Kanchan Kumar Tiwari <coolkanchan13@gmail.com>
2019-10-18 14:26:18 +0530

    Update keys.js
    removed my credentials from the project due to security reasons

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports.keys={
 
-    MONGO:"mongodb+srv://kanchan:HGSc0O8qwkwEeG9q@clustergraphql-lfk88.mongodb.net/events-graphql?retryWrites=true&w=majority"
+    MONGO:"mongodb+srv://<userName>:<password>@clustergraphql-lfk88.mongodb.net/events-graphql?retryWrites=true&w=majority"
 }
+//include your url for connection through mongodb Atlas or mongodb compass too
